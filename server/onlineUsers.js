1895fb83de4aa0205728efa192a07c1f31d47df6
adrewn <huuhung1999@gmail.com>
2021-08-05 01:03:50 -0400

    socket improvement

--- server/onlineUsers.js
+++ server/onlineUsers.js
@@ -1,2 +1,28 @@
-const onlineUsers = [];
-module.exports = onlineUsers;
+const onlineUsers = {};
+
+const checkOnlineUser = (userId) => {
+  return onlineUsers.hasOwnProperty(userId);
+};
+
+const removeOnlineUser = (userId) => {
+  return delete onlineUsers[userId];
+};
+
+const addOnlineUser = (userId, socketId) => {
+  if (checkOnlineUser(userId)) {
+    return onlineUsers[userId].push(userId);
+  } else {
+    return (onlineUsers[userId] = [socketId]);
+  }
+};
+
+const getSocketId = (userId) => {
+  return onlineUsers[userId];
+};
+
+module.exports = {
+  checkOnlineUser,
+  removeOnlineUser,
+  addOnlineUser,
+  getSocketId,
+};
---
+++ server/socketHandler.js
@@ -0,0 +1,82 @@
+const cookie = require("cookie");
+const jwt = require("jsonwebtoken");
+const { Conversation } = require("./db/models");
+
+const {
+  checkOnlineUser,
+  removeOnlineUser,
+  addOnlineUser,
+  getSocketId,
+} = require("./onlineUsers");
+
+const socketHandler = (server) => {
+  const io = require("socket.io")(server, {
+    cors: {
+      origin: process.env.REQUEST_ORIGIN,
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("go-online", async (id) => {
+      if (!checkOnlineUser(id)) {
+        addOnlineUser(id, socket.id);
+      }
+
+      joinRooms(socket, id);
+    });
+
+    socket.on("new-message", (data) => {
+      sendMessage(io, socket, data);
+    });
+
+    socket.on("logout", (id) => {
+      if (checkOnlineUser(id)) {
+        removeOnlineUser(id);
+        exitRooms(socket, id);
+      }
+    });
+  });
+};
+
+const sendMessage = (io, socket, data) => {
+  const message = data.message;
+  const room = message.conversationId.toString();
+
+  socket.join(room);
+  if (checkOnlineUser(data.recipientId)) {
+    for (let socketId of getSocketId(data.recipientId)) {
+      io.sockets.connected[socketId].join(room);
+    }
+  }
+
+  socket.to(room).emit("new-message", {
+    message: data.message,
+    sender: data.sender,
+  });
+};
+
+const exitRooms = (socket, userId) => {
+  for (let room of socket.rooms) {
+    socket.to(room).emit("remove-offline-user", userId);
+  }
+};
+
+const joinRooms = (socket, userId) => {
+  Conversation.listConversations(userId)
+    .then((conversations) => {
+      const rooms = conversations.map((conversation) => {
+        return conversation.id.toString();
+      });
+      socket.join(rooms);
+
+      for (let room of rooms) {
+        socket.to(room).emit("add-online-user", userId);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+module.exports = socketHandler;
